93953b366cb70fd3d8626c45d32000c240d4a5aa
Steve Herzog <srherzog@gmail.com>
2016-08-01 09:59:33 -0500

    Cleaned up display, fixed death alert

--- web/index.php
+++ web/index.php
@@ -17,11 +17,11 @@
 <p>You are the white/blue square. WASD to move, E to stand still, G to pick up treasure (after bumping into it). Red and pink squares are monsters, which can be fought for experience. Green squares are food, yellow squares are treasure. Experience increases your damage and your maximum HP.</p>
 
 <p>Clicking on a square will give you information about that square in the JavaScript console (so you can cheat).</p>
-<?php
+<!-- <?php
 $file=fopen("logs.txt","r") or die ("Log file read error");
 echo fread($file,filesize("logs.txt"));
 fclose($file);
-?>
+?> -->
 </div>
 <script src='https://fb.me/react-15.1.0.min.js'></script>
 <script src='https://fb.me/react-dom-15.1.0.min.js'></script>
